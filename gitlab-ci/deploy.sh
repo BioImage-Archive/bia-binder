944e276a20841e4eb6bb3896c2b6ffe3a062f431
Simon Li <spli@dundee.ac.uk>
2018-04-09 12:02:37 +0100

    Use deployment for staging/prod. Add application label

--- gitlab-ci/deploy.sh
+++ gitlab-ci/deploy.sh
@@ -4,13 +4,13 @@ set -eu
 
 # Required environment variables:
 test -n "$CI_ENVIRONMENT_NAME"
-DEPLOYMENT_NAME="${CI_ENVIRONMENT_NAME#*-}"
-test -n "$DEPLOYMENT_NAME"
+APPLICATION_NAME="${CI_ENVIRONMENT_NAME#*-}"
+test -n "$APPLICATION_NAME"
 # Since we're running inside a Kubernetes pod we can use internal kube-dns
-JUPYTERHUB_URL="http://proxy-public.jupyterhub-${DEPLOYMENT_NAME}/${DEPLOYMENT_NAME}"
+JUPYTERHUB_URL="http://proxy-public.jupyterhub-${APPLICATION_NAME}/${APPLICATION_NAME}"
 
 
-helmfile --hide-args --selector deployment=${DEPLOYMENT_NAME} sync --args '--wait --timeout=1200'
+helmfile --hide-args --selector application=${APPLICATION_NAME} sync --args '--wait --timeout=1200'
 
 
 # Is this necessary? Or should helm --wait take care of this?
