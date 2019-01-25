bcfff0b8cef91623dd5f9c94448110badfe0c5a4
Simon Li <spli@dundee.ac.uk>
2019-01-25 17:21:57 +0000

    Add helper scripts and vars template for minikube testing

---
+++ run-with-minikube.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# A helper script to start a Docker environment with kubectl helm helmfile
+# preinstalled and configured for connecting to minikube
+
+if [ "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" != "$PWD" ]; then
+    echo Must be run from script parent directory
+    exit 2
+fi
+
+exec docker run -it --rm \
+    -v "$HOME/.kube:/home/kube/.kube:ro" \
+    -v "$PWD:/k8s:ro" -w /k8s \
+    -v "$HOME/.minikube:$HOME/.minikube:ro" \
+    imagedata/kube-helm-docker:0.2.0 "$@"
